00292f45ab7fa2558e92647a4f148f6c2477d530
quivocal <quivocal@users.noreply.github.com>
2015-04-27 00:04:06 +1200

    add ranger configs

---
+++ ranger/.config/ranger/commands.py
@@ -0,0 +1,15 @@
+#
+# ~/.config/ranger/commands.py
+# Author: Dustin Tran <dustinvtran.com>
+#
+
+from ranger.api.commands import *
+
+class emptytrash(Command):
+    """:trash-empty
+
+    Runs the command without requiring shell.
+    """
+
+    def exectute(self):
+        self.fm.run("trash-empty")
